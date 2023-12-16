808521365eaaae389790523ec9224948c8e374ad
Canaga81 <Canagamemmedov@gmail.com>
2023-12-16 16:51:57 +0400

    Story Style Finish

--- src/pages/Host/Host.jsx
+++ src/pages/Host/Host.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
 import Welcome from '../../components/Welcome/Welcome'
 import Companies from '../../components/Companies/Companies'
 import Dream from '../../components/Dream/Dream'
 import Vlogger from '../../components/Vlogger/Vlogger'
+import BigCards from '../../components/BigCards/BigCards'
+import Story from '../../components/Story/Story'
 
 const Host = () => {
 
   return (
 
     <>
-    
+
       <Welcome />
       <Companies />
       <Dream />
       <Vlogger />
+      <BigCards />
+      <Story />
 
     </>
 
