74131c89e53ae24958acf890b0e67a6ea85e4cdd
Canaga81 <Canagamemmedov@gmail.com>
2023-12-17 19:03:56 +0400

    Makers Style Finish

--- src/components/DetailedCard/DetailedCard.jsx
+++ src/components/DetailedCard/DetailedCard.jsx
@@ -1,34 +1,43 @@
 import React from 'react'
 import './DetailedCard.css'
+import Makers from '../Makers/Makers'
 
 const DetailedCard = (props) => {
 
     const { inCard } = props
 
     return (
-        <section className='detailedCard'>
-            <div className="detailedCard_container">
-                <div className="detailedCard_container-left">
-                    <div className="detailedCard_container-left_image">
-                        <img src={inCard.image} alt="" />
-                    </div>
-                </div>
 
-                <div className="detailedCard_container-right">
-                    <div className="detailed_container_title_text">
-                        <p className='detailedCard_blue'>{inCard.category}</p>
-                        <h2 className='detailedCard_title_h2'>{inCard.title}</h2>
+        <>
+            <section className='detailedCard'>
+                <div className="detailedCard_container">
+                    <div className="detailedCard_container-left">
+                        <div className="detailedCard_container-left_image">
+                            <img src={inCard.image} alt="" />
+                        </div>
                     </div>
-                    <div className="detailed_desc">
-                        <p className='detailed_desc_p'>{inCard.desc}</p>
+
+                    <div className="detailedCard_container-right">
+                        <div className="detailed_container_title_text">
+                            <p className='detailedCard_blue'>{inCard.category}</p>
+                            <h2 className='detailedCard_title_h2'>{inCard.title}</h2>
+                        </div>
+                        <div className="detailed_desc">
+                            <p className='detailed_desc_p'>{inCard.desc}</p>
+                        </div>
+                        <button className="detailed_container-button">
+                            <p className="detailed_container-btn_text">Listen now</p>
+                            <p className="detailed_time">53:32</p>
+                        </button>
                     </div>
-                    <button className="detailed_container-button">
-                        <p className="detailed_container-btn_text">Listen now</p>
-                        <p className="detailed_time">53:32</p>
-                    </button>
                 </div>
-            </div>
-        </section>
+            </section>
+
+            <Makers />
+        </>
+
+
+
     )
 }
 
