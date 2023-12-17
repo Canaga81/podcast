f528f0f36dab5b0cb7b61ebe0b29364492cc3c85
Canaga81 <Canagamemmedov@gmail.com>
2023-12-17 14:02:57 +0400

    Filter JSX Style Finish

---
+++ src/components/FilterButtons/FilterButtons.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import CardsData from '../../images/CardsData'
+import './FilterButtons.css'
+
+const FilterButtons = ({ menuItems, filterItems, setItem }) => {
+
+    return (
+        <div className='filterButtons'>
+            
+            <button onClick={() => setItem(CardsData)} className='btn_filter_cards'>
+                All Podcasts
+            </button>
+
+            {
+                menuItems.map((val, i) => (
+                    <button key={i} className='btn_filter_cards' onClick={() => filterItems(val)}>
+                        {val}
+                    </button>
+                ))
+            }
+
+        </div>
+    )
+
+}
+
+export default FilterButtons
--- src/pages/Podcasts/Podcasts.jsx
+++ src/pages/Podcasts/Podcasts.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Studios from '../../components/Studios/Studios'
+import Episodes from '../../components/Episodes/Episodes'
+import Filter from '../../components/Filter/Filter'
 
 const Podcasts = () => {
   return (
     <>
       <Studios />
+      <Episodes />
+      <Filter />
     </>
   )
 }
