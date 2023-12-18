720f3cae228dc83b4291be5e239dd6e6290f45dc
Canaga81 <Canagamemmedov@gmail.com>
2023-12-18 16:58:12 +0400

    BlogAtmosphere Style Finish

--- src/components/BlogAtmosphere/BlogAtmosphere.jsx
+++ src/components/BlogAtmosphere/BlogAtmosphere.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import './BlogAtmosphere.css'
+import Makers from '../Makers/Makers'
+import InformationPerson from '../InformationPerson/InformationPerson'
+import Read from '../Read/Read'
 
 const BlogAtmosphere = (props) => {
 
     const { allblogitem } = props
 
     return (
-        <div>BlogAtmosphere</div>
+        <>
+
+            <section className='blogAtmosphere'>
+                <div className="blogAtmosphere_container">
+                    <div className="blogAtmosphere_text_cont">
+                        <p className='blogAtmosphere_text_blue'>12 Sep 2021</p>
+                        <h2 className='blogAtmosphere_title'>
+                            Apparently we had reached a great height in the atmosphere,
+                            for the sky was a dead black, and the stars.
+                        </h2>
+                    </div>
+
+                    <div className="blogAtmosphere_image">
+                        <img className='blogAtmosphere_img' src={allblogitem.image} alt="" />
+                    </div>
+                </div>
+            </section>
+
+            <Makers />
+            <InformationPerson />
+            <Read />
+
+        </>
     )
 }
 
-export default BlogAtmosphere
+export default BlogAtmosphere;
